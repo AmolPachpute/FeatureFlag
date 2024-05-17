5a567c4da7b215fdd6f3fd6e1d8ee6d3109f7129
Amol P <amolpachpute94.com>
2024-05-17 17:15:28 +0530

    Added app

--- FeatureFlag/urls.py
+++ FeatureFlag/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
+    path("api/", include("configflags.urls")),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
